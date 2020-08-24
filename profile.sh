a01f491638362d218449afddbe9e05bd1dc1e715
Colin Cheng <zbinlin@gmail.com>
2020-08-24 20:22:06 +0800

    FEAT{env}: adds CARGO_HOME and RUSTUP_HOME env

--- profile.sh
+++ profile.sh
@@ -3,15 +3,22 @@
 # source <<REPO_DIR>>/profile.sh
 #
 
-set -e
-
-append_path="${HOME}/.local/bin"
-if [ -d "${append_path}" ] && ! echo ${PATH} | awk -v p=${append_path} 'BEGIN { RS=":"; exists = 1 } ($0 == p) { exists = 0 } END { exit exists }';
-then
-    PATH="${append_path}:${PATH}"
-fi
-
 DIR=$(dirname $(readlink -f ${BASH_SOURCE:-$0}))
 . $DIR/profile_env
+
+append_paths=$(
+cat <<-EOF |
+    ${HOME}/.local/bin
+    ${CARGO_HOME:-${HOME}/.cargo}/bin
+EOF
+while read -r pth || [ -n "$pth" ];
+do
+    if [ -d "${pth}" ] && echo ${PATH} | awk -v p=${pth} 'BEGIN { RS=":" } ($0 == p) { exit 1 }';
+    then
+        printf "$pth:"
+    fi
+done
+)
+PATH="${append_paths}${PATH}"
 
 $DIR/tmpdisk.sh "${HOME}/tmp"
